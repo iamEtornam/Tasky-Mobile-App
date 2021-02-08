eaf8278308900e9e7dd794dabce38b8dc524e263
Etornam <sunumacbright@gmail.com>
2021-02-08 16:21:45 +0000

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:tasky_app/views/inbox/create_inbox_view.dart';
 
 import 'utils/custom_colors.dart';
+import 'views/account/account_view.dart';
+import 'views/auth/login_view.dart';
+import 'views/auth/organization_view.dart';
 import 'views/dashboard/dashboard_view.dart';
 import 'views/task/create_task_view.dart';
 
@@ -11,6 +15,15 @@ class Routes {
         return MaterialPageRoute(builder: (_) => DashboardView());
       case '/createNewTaskView':
         return MaterialPageRoute(builder: (_) => CreateNewTaskView());
+      case '/loginView':
+        return MaterialPageRoute(builder: (_) => LoginView());
+      case '/organizationView':
+        return MaterialPageRoute(builder: (_) => OrganizationView());
+      case '/accountView':
+        return MaterialPageRoute(builder: (_) => AccountView());
+      case '/createInboxView':
+        return MaterialPageRoute(
+            builder: (_) => CreateInboxView(), fullscreenDialog: true);
       default:
         return MaterialPageRoute(
             builder: (context) => Scaffold(

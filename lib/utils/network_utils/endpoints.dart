b2ef0d3684b0136f2a65236065668f14dfb9f4ed
Etornam <sunumacbright@gmail.com>
2021-02-10 00:53:32 +0000

    Update endpoints.dart

--- lib/utils/network_utils/endpoints.dart
+++ lib/utils/network_utils/endpoints.dart
@@ -1,2 +1,4 @@
 final String baseUrl = 'http://localhost:3000/dev';
-final String loginUrl = '$baseUrl/login';
+final String loginPath = '$baseUrl/login';
+final String organizationPath = '$baseUrl/getOrganizationById';
+final String updateDepartmentPath = '$baseUrl/updateUserDepartment';

da8f46fe39790f4725d97eb82c0418eddd685517
Etornam <sunumacbright@gmail.com>
2021-02-10 00:54:03 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:tasky_app/managers/auth_manager.dart';
+import 'package:tasky_app/managers/user_manager.dart';
 import 'package:tasky_app/routes.dart';
 import 'package:tasky_app/services/auth_service.dart';
+import 'package:tasky_app/services/organization_service.dart';
+import 'package:tasky_app/utils/local_storage.dart';
 import 'package:tasky_app/utils/network_utils/custom_http_client.dart';
-
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'managers/organization_manager.dart';
+import 'services/user_service.dart';
 import 'shared_widgets/custom_theme.dart';
+import 'views/auth/login_view.dart';
+import 'views/dashboard/dashboard_view.dart';
 
 GetIt locator = GetIt.instance;
 
@@ -16,12 +26,20 @@ setupSingletons() async {
   locator.registerLazySingleton<CustomHttpClient>(() => CustomHttpClient());
   locator.registerLazySingleton<AuthService>(() => AuthService());
   locator.registerLazySingleton<AuthManager>(() => AuthManager());
+  locator.registerLazySingleton<LocalStorage>(() => LocalStorage());
+  locator
+      .registerLazySingleton<OrganizationService>(() => OrganizationService());
+  locator
+      .registerLazySingleton<OrganizationManager>(() => OrganizationManager());
+  locator.registerLazySingleton<UserService>(() => UserService());
+  locator.registerLazySingleton<UserManager>(() => UserManager());
 }
 
 main() async {
   await setupSingletons();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Hive.initFlutter();
   runApp(MyApp());
 }
 
@@ -29,19 +47,37 @@ class MyApp extends StatelessWidget {
   final FirebaseAnalytics _analytics = FirebaseAnalytics();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tasky',
-      theme: customLightTheme(context),
-      darkTheme: customDarkTheme(context),
-      themeMode: ThemeMode.system,
-      onGenerateRoute: Routes.generateRoute,
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/loginView',
-      navigatorObservers: [FirebaseAnalyticsObserver(analytics: _analytics)],
-      localeResolutionCallback:
-          (Locale locale, Iterable<Locale> supportedLocales) {
-        return locale;
-      },
-    );
+    return StreamBuilder<User>(
+        stream: auth.authStateChanges(),
+        builder: (context, snapshot) {
+          return MaterialApp(
+            title: 'Tasky',
+            builder: BotToastInit(),
+            theme: customLightTheme(context),
+            darkTheme: customDarkTheme(context),
+            themeMode: ThemeMode.system,
+            onGenerateRoute: Routes.generateRoute,
+            debugShowCheckedModeBanner: false,
+            onGenerateInitialRoutes: (_) {
+              if (snapshot.data != null) {
+                return <Route>[
+                  MaterialPageRoute(builder: (context) => DashboardView())
+                ];
+              } else {
+                return <Route>[
+                  MaterialPageRoute(builder: (context) => LoginView())
+                ];
+              }
+            },
+            navigatorObservers: [
+              FirebaseAnalyticsObserver(analytics: _analytics),
+              BotToastNavigatorObserver(),
+            ],
+            localeResolutionCallback:
+                (Locale locale, Iterable<Locale> supportedLocales) {
+              return locale;
+            },
+          );
+        });
   }
 }

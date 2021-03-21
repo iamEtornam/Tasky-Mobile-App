6288ac781188aed4b735fc2ba0295e7d3eb6ece7
Etornam <sunumacbright@gmail.com>
2021-03-21 14:35:37 +0000

    Update organization_service.dart

--- lib/services/organization_service.dart
+++ lib/services/organization_service.dart
@@ -1,13 +1,52 @@
+import 'dart:io';
+
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
+import 'package:path/path.dart';
+import 'package:tasky_app/utils/local_storage.dart';
 import 'package:tasky_app/utils/network_utils/custom_http_client.dart';
 import 'package:tasky_app/utils/network_utils/endpoints.dart';
+import 'package:http_parser/http_parser.dart';
 
 final CustomHttpClient _customHttpClient = GetIt.I.get<CustomHttpClient>();
+final LocalStorage _localStorage = GetIt.I.get<LocalStorage>();
+
 class OrganizationService {
-  
   Future<Response> getOrganizationRequest({int organizationID}) async {
     return await _customHttpClient
         .getRequest('$organizationPath/$organizationID');
+  }
+
+  Future<Response> createOrganizationRequest(
+      {String imageUrl, String name, List<String> department}) async {
+    Map<String, dynamic> body = {
+      "name": name,
+      "logo": imageUrl,
+      "department": department
+    };
+    print(body);
+    return await _customHttpClient.postRequest(
+        path: createOrganizationPath, body: body);
+  }
+
+  Future<Response> fileUploaderRequest({File file}) async {
+    final token = await _localStorage.authToken();
+    Map<String, String> headers = {
+      "Accept": "application/json",
+      "Authorization": "Bearer $token"
+    };
+    final multipartRequest =
+        MultipartRequest('POST', Uri.parse(fileUploaderPath));
+    String fileName = basename(file.path);
+    multipartRequest.headers.addAll(headers);
+    multipartRequest.files.add(
+      await MultipartFile.fromPath('file', file.path,
+          filename: fileName, contentType: MediaType("image", "png")),
+    );
+
+    Response response = await Response.fromStream(await multipartRequest.send())
+        .timeout(Duration(seconds: 120));
+
+    return response;
   }
 }

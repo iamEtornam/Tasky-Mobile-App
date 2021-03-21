d7fae8d1010510ae485338bac54341b01d4fe71f
Etornam <sunumacbright@gmail.com>
2021-03-21 14:35:50 +0000

    Update endpoints.dart

--- lib/utils/network_utils/endpoints.dart
+++ lib/utils/network_utils/endpoints.dart
@@ -1,4 +1,10 @@
-final String baseUrl = 'http://localhost:3000/dev';
+final String baseUrl =
+    'https://3so6qpofn6.execute-api.us-east-1.amazonaws.com/prod';
 final String loginPath = '$baseUrl/login';
 final String organizationPath = '$baseUrl/getOrganizationById';
 final String updateDepartmentPath = '$baseUrl/updateUserDepartment';
+final String createOrganizationPath = '$baseUrl/createOrganization';
+final String getUserInformationPath = '$baseUrl/getUserInformation';
+final String inviteMembersPath = '$baseUrl/inviteMembers';
+final String fileUploaderPath =
+    'http://tasty-file-uploader.us-east-2.elasticbeanstalk.com/api/v0/upload';

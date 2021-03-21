c82d63fb38138e94d067f50dffa18d66a7015083
Etornam <sunumacbright@gmail.com>
2021-03-21 18:24:31 +0000

    Create task_service.dart

---
+++ lib/services/task_service.dart
@@ -0,0 +1,30 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
+import 'package:tasky_app/utils/network_utils/custom_http_client.dart';
+import 'package:tasky_app/utils/network_utils/endpoints.dart';
+
+final CustomHttpClient _customHttpClient = GetIt.I.get<CustomHttpClient>();
+
+class TaskService {
+  
+  Future<Response> createTaskRequest(
+      {String description,
+      String dueDate,
+      String department,
+      bool isReminder,
+      int organizationId,
+      int createdBy,
+      List<Map<String, dynamic>> assignees}) async {
+    Map<String, dynamic> body = {
+      'description': description,
+      'due_date': dueDate,
+      'is_reminder': isReminder,
+      'assignees': assignees,
+      'organization_id': organizationId,
+      'created_by': createdBy,
+      'department': department
+    };
+    return await _customHttpClient.postRequest(
+        path: createTaskPath, body: body);
+  }
+}

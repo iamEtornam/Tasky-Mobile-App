a26ac1d36969696e4c5de6ea457b46036ceb8714
Etornam <sunumacbright@gmail.com>
2021-03-21 18:23:27 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,16 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:tasky_app/managers/auth_manager.dart';
+import 'package:tasky_app/managers/task_manager.dart';
 import 'package:tasky_app/managers/user_manager.dart';
 import 'package:tasky_app/routes.dart';
 import 'package:tasky_app/services/auth_service.dart';
 import 'package:tasky_app/services/organization_service.dart';
+import 'package:tasky_app/services/task_service.dart';
 import 'package:tasky_app/utils/local_storage.dart';
 import 'package:tasky_app/utils/network_utils/custom_http_client.dart';
 import 'package:hive/hive.dart';
@@ -17,22 +20,27 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'managers/organization_manager.dart';
 import 'services/user_service.dart';
 import 'shared_widgets/custom_theme.dart';
-import 'views/auth/login_view.dart';
-import 'views/dashboard/dashboard_view.dart';
 
 GetIt locator = GetIt.instance;
 
 setupSingletons() async {
   locator.registerLazySingleton<CustomHttpClient>(() => CustomHttpClient());
+
   locator.registerLazySingleton<AuthService>(() => AuthService());
   locator.registerLazySingleton<AuthManager>(() => AuthManager());
+
   locator.registerLazySingleton<LocalStorage>(() => LocalStorage());
+
   locator
       .registerLazySingleton<OrganizationService>(() => OrganizationService());
   locator
       .registerLazySingleton<OrganizationManager>(() => OrganizationManager());
+
   locator.registerLazySingleton<UserService>(() => UserService());
   locator.registerLazySingleton<UserManager>(() => UserManager());
+
+  locator.registerLazySingleton<TaskService>(() => TaskService());
+  locator.registerLazySingleton<TaskManager>(() => TaskManager());
 }
 
 main() async {
@@ -49,7 +57,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User>(
-        stream: _auth.authStateChanges(),
+        stream: _auth.userChanges(),
         builder: (context, snapshot) {
           return MaterialApp(
             title: 'Tasky',
@@ -57,20 +65,9 @@ class MyApp extends StatelessWidget {
             theme: customLightTheme(context),
             darkTheme: customDarkTheme(context),
             themeMode: ThemeMode.system,
+            initialRoute: '/',
             onGenerateRoute: Routes.generateRoute,
             debugShowCheckedModeBanner: false,
-            onGenerateInitialRoutes: (_) {
-              print(snapshot.data);
-              if (snapshot.data != null) {
-                return <Route>[
-                  MaterialPageRoute(builder: (context) => DashboardView())
-                ];
-              } else {
-                return <Route>[
-                  MaterialPageRoute(builder: (context) => LoginView())
-                ];
-              }
-            },
             navigatorObservers: [
               FirebaseAnalyticsObserver(analytics: _analytics),
               BotToastNavigatorObserver(),

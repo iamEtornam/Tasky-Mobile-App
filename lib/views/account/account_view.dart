9f17ea52c7a33e852b2cd865e8c98ec91eaef485
Etornam <sunumacbright@gmail.com>
2021-03-21 14:35:56 +0000

    Update account_view.dart

--- lib/views/account/account_view.dart
+++ lib/views/account/account_view.dart
@@ -1,11 +1,25 @@
 import 'dart:io';
 import 'dart:math';
 
+import 'package:bot_toast/bot_toast.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fb;
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:get_it/get_it.dart';
+import 'package:tasky_app/managers/user_manager.dart';
+import 'package:tasky_app/models/user.dart';
 import 'package:tasky_app/utils/ui_utils/custom_colors.dart';
+import 'package:textfield_tags/textfield_tags.dart';
 
-class AccountView extends StatelessWidget {
+final UserManager _userManager = GetIt.I.get<UserManager>();
+
+class AccountView extends StatefulWidget {
+  @override
+  _AccountViewState createState() => _AccountViewState();
+}
+
+class _AccountViewState extends State<AccountView> {
+  List<String> departments = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,76 +35,271 @@ class AccountView extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(24),
         children: [
-          Center(
-            child: CircleAvatar(
-              backgroundColor: Colors
-                  .primaries[Random().nextInt(Colors.primaries.length)]
-                  .withOpacity(.2),
-              radius: 60,
-              backgroundImage: ExactAssetImage('assets/avatar.png'),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Center(
-              child: Text(
-            'Bright Sunu Etornam',
-            style: Theme.of(context)
-                .textTheme
-                .headline6
-                .copyWith(fontWeight: FontWeight.w600),
-          )),
-          SizedBox(
-            height: 10,
-          ),
-          Center(
-              child: Text(
-            'View Profile',
-            style: Theme.of(context)
-                .textTheme
-                .bodyText2
-                .copyWith(fontWeight: FontWeight.normal, color: customRedColor),
-          )),
-          SizedBox(
-            height: 25,
-          ),
-          Text(
-            'Organizations',
-            style: Theme.of(context).textTheme.bodyText1.copyWith(
-                fontWeight: FontWeight.normal, color: customGreyColor),
-          ),
-          Material(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            child: ListTile(
-              title: Text(
-                'Aura Innovations',
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-              subtitle: Text(
-                'info@etornam.dev',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText2
-                    .copyWith(color: customGreyColor),
-              ),
-              trailing: TextButton(
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    backgroundColor: customRedColor,
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    'Invite',
-                    style: Theme.of(context)
-                        .textTheme
-                        .button
-                        .copyWith(color: Colors.white),
-                  )),
-            ),
-          ),
+          StreamBuilder<User>(
+              stream: _userManager.getUserInformation().asStream(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Column(
+                    children: [
+                      Center(
+                        child: CircleAvatar(
+                          backgroundColor: Colors.primaries[
+                                  Random().nextInt(Colors.primaries.length)]
+                              .withOpacity(.2),
+                          radius: 60,
+                          backgroundImage: ExactAssetImage('assets/avatar.png'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Center(
+                          child: Text(
+                        'Full Name here',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            .copyWith(fontWeight: FontWeight.w600),
+                      )),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Center(
+                          child: Text(
+                        'View Profile',
+                        style: Theme.of(context).textTheme.bodyText2.copyWith(
+                            fontWeight: FontWeight.normal,
+                            color: customRedColor),
+                      )),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Text(
+                        'Organizations',
+                        style: Theme.of(context).textTheme.bodyText1.copyWith(
+                            fontWeight: FontWeight.normal,
+                            color: customGreyColor),
+                      ),
+                      Material(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: ListTile(
+                          title: Text(
+                            'My Company',
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                          subtitle: Text(
+                            'info@mail.com',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText2
+                                .copyWith(color: customGreyColor),
+                          ),
+                          trailing: TextButton(
+                              style: TextButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30)),
+                                backgroundColor: customRedColor,
+                              ),
+                              onPressed: () {},
+                              child: Text(
+                                'Invite',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .button
+                                    .copyWith(color: Colors.white),
+                              )),
+                        ),
+                      ),
+                    ],
+                  );
+                }
+                return Column(
+                  children: [
+                    Center(
+                      child: CircleAvatar(
+                        backgroundColor: Colors.primaries[
+                                Random().nextInt(Colors.primaries.length)]
+                            .withOpacity(.2),
+                        radius: 60,
+                        backgroundImage: snapshot.data == null
+                            ? ExactAssetImage('assets/avatar.png')
+                            : NetworkImage(snapshot.data.data.picture),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                        child: Text(
+                      '${snapshot.data.data.name}',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          .copyWith(fontWeight: FontWeight.w600),
+                    )),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Center(
+                        child: Text(
+                      'View Profile',
+                      style: Theme.of(context).textTheme.bodyText2.copyWith(
+                          fontWeight: FontWeight.normal, color: customRedColor),
+                    )),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                      'Organizations',
+                      style: Theme.of(context).textTheme.bodyText1.copyWith(
+                          fontWeight: FontWeight.normal,
+                          color: customGreyColor),
+                    ),
+                    Material(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: ListTile(
+                        title: Text(
+                          '${snapshot.data?.data?.organization?.name}',
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                        subtitle: Text(
+                          '${snapshot.data?.data?.email}',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText2
+                              .copyWith(color: customGreyColor),
+                        ),
+                        trailing: TextButton(
+                            style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30)),
+                              backgroundColor: customRedColor,
+                            ),
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  child: AlertDialog(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    content: SizedBox(
+                                      height: 150,
+                                      child: Column(
+                                        children: [
+                                          TextFieldTags(
+                                            textFieldStyler: TextFieldStyler(
+                                              helperText: '',
+                                              cursorColor: Theme.of(context)
+                                                  .textSelectionTheme
+                                                  .cursorColor,
+                                              hintStyle: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyText1
+                                                  .copyWith(color: Colors.grey),
+                                              hintText: 'Emails',
+                                              textFieldBorder:
+                                                  UnderlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                          color:
+                                                              customGreyColor)),
+                                              textStyle: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyText1,
+                                              textFieldEnabledBorder:
+                                                  UnderlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                          color:
+                                                              customGreyColor)),
+                                              textFieldFocusedBorder:
+                                                  UnderlineInputBorder(
+                                                      borderSide: BorderSide(
+                                                          color:
+                                                              customGreyColor)),
+                                            ),
+                                            //[tagsStyler] is required and shall not be null
+                                            tagsStyler: TagsStyler(
+                                              //These are properties you can tweek for customization of tags
+                                              tagTextStyle: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyText1
+                                                  .copyWith(
+                                                      color: Colors.white),
+                                              tagDecoration: BoxDecoration(
+                                                color: customRedColor,
+                                                borderRadius:
+                                                    BorderRadius.circular(5.0),
+                                              ),
+                                              tagCancelIcon: Icon(Icons.cancel,
+                                                  size: 18.0,
+                                                  color: Colors.white),
+                                              tagPadding:
+                                                  const EdgeInsets.all(8.0),
+
+                                              // EdgeInsets tagPadding = const EdgeInsets.all(4.0),
+                                              // EdgeInsets tagMargin = const EdgeInsets.symmetric(horizontal: 4.0),
+                                              // BoxDecoration tagDecoration = const BoxDecoration(color: Color.fromARGB(255, 74, 137, 92)),
+                                              // TextStyle tagTextStyle,
+                                              // Icon tagCancelIcon = const Icon(Icons.cancel, size: 18.0, color: Colors.green)
+                                              // isHashTag: true,
+                                            ),
+                                            onTag: (tag) {
+                                              //This give you tags entered
+                                              print('onTag ' + tag);
+                                              setState(() {
+                                                departments.add(tag);
+                                              });
+                                            },
+                                            onDelete: (tag) {
+                                              print('onDelete ' + tag);
+                                              setState(() {
+                                                departments.remove(tag);
+                                              });
+                                            },
+                                          ),
+                                          SizedBox(
+                                            height: 10,
+                                          ),
+                                          TextButton(
+                                              style: TextButton.styleFrom(
+                                                  backgroundColor:
+                                                      customRedColor,
+                                                  shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            8),
+                                                  )),
+                                              onPressed: () {},
+                                              child: Padding(
+                                                padding: const EdgeInsets.only(
+                                                    left: 15, right: 15),
+                                                child: Text(
+                                                  'Invite email(s)',
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .button
+                                                      .copyWith(
+                                                          color: Colors.white),
+                                                ),
+                                              ))
+                                        ],
+                                      ),
+                                    ),
+                                  ));
+                            },
+                            child: Text(
+                              'Invite',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .button
+                                  .copyWith(color: Colors.white),
+                            )),
+                      ),
+                    ),
+                  ],
+                );
+              }),
           SizedBox(
             height: 25,
           ),
@@ -300,7 +509,18 @@ class AccountView extends StatelessWidget {
                     borderRadius: BorderRadius.circular(8)),
                 backgroundColor: Theme.of(context).cardColor,
               ),
-              onPressed: () {},
+              onPressed: () async {
+                BotToast.showLoading(
+                    allowClick: false,
+                    clickClose: false,
+                    backButtonBehavior: BackButtonBehavior.ignore);
+                fb.FirebaseAuth firebaseAuth = fb.FirebaseAuth.instance;
+                firebaseAuth.signOut().then((_) {
+                  BotToast.closeAllLoading();
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, '/loginView', (route) => false);
+                });
+              },
               child: Text(
                 'Log out',
                 style: Theme.of(context)

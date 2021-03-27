bb450bcf8e455556be6fc2b5d8f080d450c7e633
Etornam <sunumacbright@gmail.com>
2021-03-27 21:34:18 +0000

    Create personal_account_view.dart

---
+++ lib/views/account/personal_account_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PersonalAccountView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}

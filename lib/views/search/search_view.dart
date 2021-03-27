4bc172a5aa20ba0f07dab9799fed46cad0dee385
Etornam <sunumacbright@gmail.com>
2021-03-27 21:34:21 +0000

    Create search_view.dart

---
+++ lib/views/search/search_view.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class SearchView extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      Padding(
+        padding: const EdgeInsets.only(right: 10.0),
+        child: InkWell(
+            onTap: () {
+              query = '';
+            },
+            child: Center(
+                child: Text(
+              'Clear',
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle1
+                  .copyWith(color: Colors.grey),
+            ))),
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    if (query.length < 3) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Center(
+              child: Text(
+                "Search term must be longer than two letters.",
+              ),
+            ),
+          )
+        ],
+      );
+    }
+    return Column();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Column();
+  }
+}

b96df0af3faf9731e3c7f8ec82fe355f6c85a8b9
Etornam <sunumacbright@gmail.com>
2021-09-27 21:36:05 +0000

    Update create_inbox_view.dart

--- lib/views/inbox/create_inbox_view.dart
+++ lib/views/inbox/create_inbox_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:tasky_app/utils/ui_utils/custom_colors.dart';
 
 class CreateInboxView extends StatelessWidget {
+  const CreateInboxView({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

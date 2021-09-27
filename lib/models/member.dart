5b95900e449d471b456287ebf947f3c9979f8ca8
Etornam <sunumacbright@gmail.com>
2021-09-27 21:34:53 +0000

    Update member.dart

--- lib/models/member.dart
+++ lib/models/member.dart
@@ -1,25 +1,28 @@
 import 'package:tasky_app/models/user.dart';
 
 class Member {
-    Member({
-        this.status,
-        this.message,
-        this.data,
-    });
+  Member({
+    this.status,
+    this.message,
+    this.data,
+  });
 
-    bool status;
-    String message;
-    List<Data> data;
+  bool status;
+  String message;
+  List<Data> data;
 
-    factory Member.fromMap(Map<String, dynamic> json) => Member(
-        status: json["status"] == null ? null : json["status"],
-        message: json["message"] == null ? null : json["message"],
-        data: json["data"] == null ? null : List<Data>.from(json["data"].map((x) => Data.fromMap(x))),
-    );
+  factory Member.fromMap(Map<String, dynamic> json) => Member(
+        status: json["status"],
+        message: json["message"],
+        data: json["data"] == null
+            ? null
+            : List<Data>.from(json["data"].map((x) => Data.fromMap(x))),
+      );
 
-    Map<String, dynamic> toMap() => {
-        "status": status == null ? null : status,
-        "message": message == null ? null : message,
-        "data": data == null ? null : List<Data>.from(data.map((x) => x.toMap())),
-    };
+  Map<String, dynamic> toMap() => {
+        "status": status,
+        "message": message,
+        "data":
+            data == null ? null : List<Data>.from(data.map((x) => x.toMap())),
+      };
 }

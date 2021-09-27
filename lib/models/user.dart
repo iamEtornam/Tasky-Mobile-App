88f1bbdaa3a8432eb7a2c39aa510037e787c0c53
Etornam <sunumacbright@gmail.com>
2021-09-27 21:35:03 +0000

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,96 +2,102 @@
 //
 //     final user = userFromMap(jsonString);
 
-import 'organization.dart' as Org;
+import 'organization.dart' as _org;
 
 class User {
-    User({
-        this.status,
-        this.message,
-        this.data,
-    });
+  User({
+    this.status,
+    this.message,
+    this.data,
+  });
 
-    bool status;
-    String message;
-    Data data;
+  bool status;
+  String message;
+  Data data;
 
-    factory User.fromMap(Map<String, dynamic> json) => User(
-        status: json["status"] == null ? null : json["status"],
-        message: json["message"] == null ? null : json["message"],
+  factory User.fromMap(Map<String, dynamic> json) => User(
+        status: json["status"],
+        message: json["message"],
         data: json["data"] == null ? null : Data.fromMap(json["data"]),
-    );
+      );
 
-    Map<String, dynamic> toMap() => {
-        "status": status == null ? null : status,
-        "message": message == null ? null : message,
-        "data": data == null ? null : data.toMap(),
-    };
+  Map<String, dynamic> toMap() => {
+        "status": status,
+        "message": message,
+        "data": data.toMap(),
+      };
 }
 
 class Data {
-    Data({
-        this.id,
-        this.name,
-        this.picture,
-        this.organizationId,
-        this.team,
-        this.fcmToken,
-        this.authToken,
-        this.email,
-        this.phoneNumber,
-        this.userId,
-        this.signInProvider,
-        this.createdAt,
-        this.updatedAt,
-        this.organization,
-    });
+  Data({
+    this.id,
+    this.name,
+    this.picture,
+    this.organizationId,
+    this.team,
+    this.fcmToken,
+    this.authToken,
+    this.email,
+    this.phoneNumber,
+    this.userId,
+    this.signInProvider,
+    this.createdAt,
+    this.updatedAt,
+    this.organization,
+  });
 
-    int id;
-    String name;
-    String picture;
-    int organizationId;
-    String team;
-    String fcmToken;
-    String authToken;
-    String email;
-    dynamic phoneNumber;
-    String userId;
-    String signInProvider;
-    DateTime createdAt;
-    DateTime updatedAt;
-    Org.Data organization;
+  int id;
+  String name;
+  String picture;
+  int organizationId;
+  String team;
+  String fcmToken;
+  String authToken;
+  String email;
+  dynamic phoneNumber;
+  String userId;
+  String signInProvider;
+  DateTime createdAt;
+  DateTime updatedAt;
+  _org.Data organization;
 
-    factory Data.fromMap(Map<String, dynamic> json) => Data(
-        id: json["id"] == null ? null : json["id"],
-        name: json["name"] == null ? null : json["name"],
-        picture: json["picture"] == null ? null : json["picture"],
-        organizationId: json["organizationId"] == null ? null : json["organizationId"],
-        team: json["team"] == null ? null : json["team"],
-        fcmToken: json["fcm_token"] == null ? null : json["fcm_token"],
-        authToken: json["auth_token"] == null ? null : json["auth_token"],
-        email: json["email"] == null ? null : json["email"],
+  factory Data.fromMap(Map<String, dynamic> json) => Data(
+        id: json["id"],
+        name: json["name"],
+        picture: json["picture"],
+        organizationId: json["organizationId"],
+        team: json["team"],
+        fcmToken: json["fcm_token"],
+        authToken: json["auth_token"],
+        email: json["email"],
         phoneNumber: json["phone_number"],
-        userId: json["user_id"] == null ? null : json["user_id"],
-        signInProvider: json["sign_in_provider"] == null ? null : json["sign_in_provider"],
-        createdAt: json["createdAt"] == null ? null : DateTime.parse(json["createdAt"]),
-        updatedAt: json["updatedAt"] == null ? null : DateTime.parse(json["updatedAt"]),
-        organization: json["organization"] == null ? null : Org.Data.fromMap(json["organization"]),
-    );
+        userId: json["user_id"],
+        signInProvider: json["sign_in_provider"],
+        createdAt: json["createdAt"] == null
+            ? null
+            : DateTime.parse(json["createdAt"]),
+        updatedAt: json["updatedAt"] == null
+            ? null
+            : DateTime.parse(json["updatedAt"]),
+        organization: json["organization"] == null
+            ? null
+            : _org.Data.fromMap(json["organization"]),
+      );
 
-    Map<String, dynamic> toMap() => {
-        "id": id == null ? null : id,
-        "name": name == null ? null : name,
-        "picture": picture == null ? null : picture,
-        "organizationId": organizationId == null ? null : organizationId,
-        "team": team == null ? null : team,
-        "fcm_token": fcmToken == null ? null : fcmToken,
-        "auth_token": authToken == null ? null : authToken,
-        "email": email == null ? null : email,
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "picture": picture,
+        "organizationId": organizationId,
+        "team": team,
+        "fcm_token": fcmToken,
+        "auth_token": authToken,
+        "email": email,
         "phone_number": phoneNumber,
-        "user_id": userId == null ? null : userId,
-        "sign_in_provider": signInProvider == null ? null : signInProvider,
-        "createdAt": createdAt == null ? null : createdAt.toIso8601String(),
-        "updatedAt": updatedAt == null ? null : updatedAt.toIso8601String(),
-        "organization": organization == null ? null : organization.toMap(),
-    };
+        "user_id": userId,
+        "sign_in_provider": signInProvider,
+        "createdAt": createdAt.toIso8601String(),
+        "updatedAt": updatedAt.toIso8601String(),
+        "organization": organization.toMap(),
+      };
 }

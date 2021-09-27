69b711d5b73cbf7c9e27b944b0ce56a38825d567
Etornam <sunumacbright@gmail.com>
2021-09-27 21:35:00 +0000

    Update task_statistic.dart

--- lib/models/task_statistic.dart
+++ lib/models/task_statistic.dart
@@ -1,48 +1,51 @@
-
 class TaskStatistic {
-    TaskStatistic({
-        this.status,
-        this.message,
-        this.data,
-    });
+  TaskStatistic({
+    this.status,
+    this.message,
+    this.data,
+  });
 
-    bool status;
-    String message;
-    List<Datum> data;
+  bool status;
+  String message;
+  List<Datum> data;
 
-    factory TaskStatistic.fromMap(Map<String, dynamic> json) => TaskStatistic(
-        status: json["status"] == null ? null : json["status"],
-        message: json["message"] == null ? null : json["message"],
-        data: json["data"] == null ? null : List<Datum>.from(json["data"].map((x) => Datum.fromMap(x))),
-    );
+  factory TaskStatistic.fromMap(Map<String, dynamic> json) => TaskStatistic(
+        status: json["status"],
+        message: json["message"],
+        data: json["data"] == null
+            ? null
+            : List<Datum>.from(json["data"].map((x) => Datum.fromMap(x))),
+      );
 
-    Map<String, dynamic> toMap() => {
-        "status": status == null ? null : status,
-        "message": message == null ? null : message,
-        "data": data == null ? null : List<dynamic>.from(data.map((x) => x.toMap())),
-    };
+  Map<String, dynamic> toMap() => {
+        "status": status,
+        "message": message,
+        "data": data == null
+            ? null
+            : List<dynamic>.from(data.map((x) => x.toMap())),
+      };
 }
 
 class Datum {
-    Datum({
-        this.todo,
-        this.inProgress,
-        this.completed,
-    });
+  Datum({
+    this.todo,
+    this.inProgress,
+    this.completed,
+  });
 
-    int todo;
-    int inProgress;
-    int completed;
+  int todo;
+  int inProgress;
+  int completed;
 
-    factory Datum.fromMap(Map<String, dynamic> json) => Datum(
-        todo: json["todo"] == null ? null : json["todo"],
-        inProgress: json["in_progress"] == null ? null : json["in_progress"],
-        completed: json["completed"] == null ? null : json["completed"],
-    );
+  factory Datum.fromMap(Map<String, dynamic> json) => Datum(
+        todo: json["todo"],
+        inProgress: json["in_progress"],
+        completed: json["completed"],
+      );
 
-    Map<String, dynamic> toMap() => {
-        "todo": todo == null ? null : todo,
-        "in_progress": inProgress == null ? null : inProgress,
-        "completed": completed == null ? null : completed,
-    };
+  Map<String, dynamic> toMap() => {
+        "todo": todo,
+        "in_progress": inProgress,
+        "completed": completed,
+      };
 }

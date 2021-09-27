61513fa493e957c5aa8a8ed3f09a4f002d7bf389
Etornam <sunumacbright@gmail.com>
2021-09-27 21:35:27 +0000

    Update endpoints.dart

--- lib/utils/network_utils/endpoints.dart
+++ lib/utils/network_utils/endpoints.dart
@@ -1,17 +1,17 @@
-final String baseUrl = 'http://localhost:3000/dev';
-final String loginPath = '$baseUrl/login';
-final String organizationPath = '$baseUrl/getOrganizationById';
-final String updateTeamPath = '$baseUrl/updateUserTeam';
-final String createOrganizationPath = '$baseUrl/createOrganization';
-final String getUserInformationPath = '$baseUrl/getUserInformation';
-final String inviteMembersPath = '$baseUrl/inviteMembers';
-final String createTaskPath = '$baseUrl/createTask';
+const String baseUrl = 'http://localhost:3000/dev';
+const String loginPath = '$baseUrl/login';
+const String organizationPath = '$baseUrl/getOrganizationById';
+const String updateTeamPath = '$baseUrl/updateUserTeam';
+const String createOrganizationPath = '$baseUrl/createOrganization';
+const String getUserInformationPath = '$baseUrl/getUserInformation';
+const String inviteMembersPath = '$baseUrl/inviteMembers';
+const String createTaskPath = '$baseUrl/createTask';
 String listMembersPath(int organizationId) =>
     '$baseUrl/listMembers/$organizationId';
-final String fileUploaderPath =
+const String fileUploaderPath =
     'http://tasty-file-uploader.us-east-2.elasticbeanstalk.com/api/v0/upload';
-final String updateTokenPath = '$baseUrl/updateUserToken';
+const String updateTokenPath = '$baseUrl/updateUserToken';
 String getTaskPath(int organizationId) => '$baseUrl/getTasks/$organizationId';
 String getTaskStatisticsPath(int userId) =>
     '$baseUrl/getTaskStatusCount/$userId';
-final String updateTaskPath = '$baseUrl/updateTask';
+const String updateTaskPath = '$baseUrl/updateTask';

a4329076a0f879a81fc00ef557894664339f61e3
Etornam <sunumacbright@gmail.com>
2021-09-27 21:35:43 +0000

    Update personal_account_view.dart

--- lib/views/account/personal_account_view.dart
+++ lib/views/account/personal_account_view.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class PersonalAccountView extends StatelessWidget {
+  const PersonalAccountView({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: ListView(
-        children: [],
+        children: const [],
       ),
     );
   }

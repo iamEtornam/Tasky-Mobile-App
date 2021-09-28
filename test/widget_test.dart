76ae1678b4caeba4a48622ffd83f5b1c068162de
Etornam <sunumacbright@gmail.com>
2021-09-28 17:12:31 +0000

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tasky_app/main.dart';
+import 'package:tasky_mobile_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

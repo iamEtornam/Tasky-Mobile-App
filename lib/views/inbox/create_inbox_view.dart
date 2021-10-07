2d739ca14bcc7b4b057dadc2bbad8f9ce0e6419d
Etornam <sunumacbright@gmail.com>
2021-10-07 11:45:53 +0000

    Update create_inbox_view.dart

--- lib/views/inbox/create_inbox_view.dart
+++ lib/views/inbox/create_inbox_view.dart
@@ -30,6 +30,52 @@ class CreateInboxView extends StatelessWidget {
               .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
+      body: ListView(
+        padding: const EdgeInsets.all(24),
+        children: [
+          Text(
+            'What is the title?',
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1
+                .copyWith(fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          TextFormField(
+            style: Theme.of(context).textTheme.bodyText1,
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.text,
+            textCapitalization: TextCapitalization.words,
+            maxLines: 1,
+            cursorColor: Theme.of(context).textSelectionTheme.cursorColor,
+            enableInteractiveSelection: true,
+            decoration: InputDecoration(
+                filled: false,
+        
+                enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: customGreyColor)),
+                focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: customGreyColor)),
+                border: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black)),
+                hintStyle: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(color: Colors.grey)),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Field cannot be Empty';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
     );
   }
 }

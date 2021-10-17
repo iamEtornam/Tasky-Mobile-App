d2dc03cca291ddc1442a3fa4fa8e0b7611442851
Etornam <sunumacbright@gmail.com>
2021-10-17 09:48:18 +0000

    Update task_statistic.dart

--- lib/models/task_statistic.dart
+++ lib/models/task_statistic.dart
@@ -38,14 +38,14 @@ class Datum {
   int? completed;
 
   factory Datum.fromMap(Map<String, dynamic> json) => Datum(
-        todo: json["todo"],
-        inProgress: json["in_progress"],
-        completed: json["completed"],
+        todo: json["todo"] ?? 0,
+        inProgress: json["in_progress"] ?? 0,
+        completed: json["completed"] ?? 0,
       );
 
   Map<String, dynamic> toMap() => {
-        "todo": todo,
-        "in_progress": inProgress,
-        "completed": completed,
+        "todo": todo ?? 0,
+        "in_progress": inProgress ?? 0,
+        "completed": completed ?? 0,
       };
 }

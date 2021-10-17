d74e5d93a1d078e70929ca0be9823fe37d6895cf
Etornam <sunumacbright@gmail.com>
2021-10-17 09:48:06 +0000

    Create file_upload_manager.dart

---
+++ lib/managers/file_upload_manager.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:tasky_mobile_app/services/file_upload_service.dart';
+
+class FileUploadManager with ChangeNotifier {
+    final FileUploadService _fileUploadService = GetIt.I.get<FileUploadService>();
+  String? _message = '';
+
+  String? get message => _message;
+
+  setMessage(String? message) {
+    _message = message;
+    notifyListeners();
+  }
+
+  Future<String?> updateOrganizationPicture(File imageFile) async {
+    try {
+      String? fileUrl;
+      await _fileUploadService
+          .fileUploaderRequest(file: imageFile)
+          .then((response) async {
+        Map<String, dynamic> body = json.decode(response.body);
+
+        if (response.statusCode == 200) {
+          fileUrl = body['fileUrl'];
+        } else {
+          fileUrl = null;
+        }
+      }).catchError((onError) {
+        setMessage('Something went wrong while uploading file');
+        fileUrl = null;
+      });
+      return fileUrl;
+    } catch (e) {
+      return null;
+    }
+  }
+}

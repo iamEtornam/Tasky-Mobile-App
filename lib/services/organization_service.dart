4ecb90dec427572bb6f5975a60e1ab4bf7b416d6
Etornam <sunumacbright@gmail.com>
2021-10-17 09:48:23 +0000

    Update organization_service.dart

--- lib/services/organization_service.dart
+++ lib/services/organization_service.dart
@@ -1,17 +1,11 @@
-import 'dart:io';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
-import 'package:http_parser/http_parser.dart';
 import 'package:logger/logger.dart';
-import 'package:path/path.dart';
 import 'package:tasky_mobile_app/utils/network_utils/custom_http_client.dart';
 import 'package:tasky_mobile_app/utils/network_utils/endpoints.dart';
 
 class OrganizationService {
   final CustomHttpClient _customHttpClient = GetIt.I.get<CustomHttpClient>();
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   final Logger _logger = Logger();
 
   Future<Response> getOrganizationRequest({int? organizationID}) async {
@@ -29,27 +23,6 @@ class OrganizationService {
     _logger.d(body);
     return await _customHttpClient.postRequest(
         path: createOrganizationPath, body: body);
-  }
-
-  Future<Response> fileUploaderRequest({required File file}) async {
-    final token = await _firebaseAuth.currentUser!.getIdToken();
-    Map<String, String> headers = {
-      "Accept": "application/json",
-      "Authorization": "Bearer $token"
-    };
-    final multipartRequest =
-        MultipartRequest('POST', Uri.parse(fileUploaderPath));
-    String fileName = basename(file.path);
-    multipartRequest.headers.addAll(headers);
-    multipartRequest.files.add(
-      await MultipartFile.fromPath('file', file.path,
-          filename: fileName, contentType: MediaType("image", "png")),
-    );
-
-    Response response = await Response.fromStream(await multipartRequest.send())
-        .timeout(const Duration(seconds: 120));
-
-    return response;
   }
 
   Future<Response> getMemberListRequest({int? organizationId}) async {
